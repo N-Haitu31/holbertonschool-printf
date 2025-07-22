78604f0e94f2f231bc12c80cc2fef4673bbac213
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-22 11:20:55 +0200

    Printf handle conversion specifiers

--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
+#include <limits.h>
 
 /**
  * _printf - copy printf function
@@ -20,7 +21,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
-	while (format != NULL && format[i] != '\0')
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1])
 		{
@@ -34,29 +35,31 @@ int _printf(const char *format, ...)
 			else if (format[i] == '%')
 				count += print_percent();
 
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_int(args);
+
 			else
 			{
 				count += write(1, "%", 1);
 				count += write(1, &format[i], 1);
 			}
 		}
-			else
-			{
-				count += write(1, &format[i], 1);
-			}
-			i++;
+		else
+		{
+			count += write(1, &format[i], 1);
+		}
+		i++;
 	}
 	va_end(args);
 	return (count);
-
 }
 
 /**
-* print_char - print character
-* @args : list arguments
-*
-* Return: 1
-*/
+ * print_char - print character
+ * @args : list arguments
+ *
+ * Return: 1
+ */
 int print_char (va_list args)
 {
 	char c = va_arg(args, int);
@@ -65,11 +68,11 @@ int print_char (va_list args)
 }
 
 /**
-* print_str - print string
-* @args : list arguments
-*
-* Return: numbers characters
-*/
+ * print_str - print string
+ * @args : list arguments
+ *
+ * Return: numbers characters
+ */
 int print_str(va_list args)
 {
 	int len = 0;
@@ -83,13 +86,60 @@ int print_str(va_list args)
 }
 
 /**
-* print_percent - print "%"
-*
-* Return: 1
-*/
+ * print_percent - print "%"
+ *
+ * Return: 1
+ */
 int print_percent(void)
 {
 	char c = '%';
 
 	return (write(1, &c, 1));
+}
+
+/**
+ * print_int - print integer
+ * @args : list arguments
+ *
+ * Return: count
+ */
+
+int print_int (va_list args)
+{
+	/*unsigned int num;*/
+	int count = 0;
+	long int n = va_arg(args, int);
+	char buffer[12];
+	int i = 0;
+
+	if (n == INT_MIN)
+	{
+		count += write(1, "-2147483648", 11);
+		return (count);
+	}
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		n = -n;
+	}
+
+	if (n == 0)
+	{
+		buffer[i++] = '0';
+	}
+	else
+	{
+		while (n > 0)
+		{
+			buffer[i++] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	while (i > 0)
+	{
+		i--;
+		write(1, &buffer[i], 1);
+	}
+
+	return (count);
 }
